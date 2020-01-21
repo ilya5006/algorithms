c3c6026e8aa519de4c8d6d5f0f9c4dca47086ddd
ilya5006 <samarcev.ilya@mail.ru>
2020-01-21 19:45:34 +0300

    Code beautify and bug fix

--- mnoj.pas
+++ mnoj.pas
@@ -1,24 +1,24 @@
-﻿uses crt;
-var
- n,i:integer;
- a:array [0..5] of integer;
+﻿var
+    n,i:integer;
+    a:array [0..5] of integer;
 begin
-n:=4;
-for i:=0 to n do a[i]:=0;
-a[n]:=1;
-while a[0]=0 do
- begin
-  write('{');
-  for i:=1 to n do
-  if a[i]=1 then write(' ',i-1);
-  write('} ');
-  writeLn('');
-  i:=n;
-  while a[i]=1 do
-   begin
-    a[i]:=0;
-    dec(i);
-   end;
-  inc(a[i]);
- end;
+    n:=5;
+    for i:=0 to n do a[i]:=0;
+    a[n]:=1;
+    while a[0]=0 do
+    begin
+        write('{');
+        for i:=1 to n do
+            if a[i]=1 then
+                write(' ',i-1);
+        write('}');
+        writeLn('');
+        i:=n;
+        while a[i]=1 do
+        begin
+            a[i]:=0;
+            dec(i);
+        end;
+        inc(a[i]);
+    end;
 end.
