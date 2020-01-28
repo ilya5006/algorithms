43da0984fe5b7d60e224af3048f6cba3485369c2
ilya5006 <samarcev.ilya@mail.ru>
2020-01-28 23:27:20 +0300

    Not done algorithm

--- maze2.js
+++ maze2.js
@@ -1,67 +1,74 @@
-let n = 11;
-let str = [];
-
-for (let i = 0; i < n; i++)
+let printArr = (arr) =>
 {
-    str[i] = i;
-}
-
-for (let i = 0; i < n + 2; i++)
-{
-    process.stdout.write('1');
-}
-process.stdout.write('\n');
-
-for (let i = 0; i < n; i++)
-{
-    // Объединение (есть стена или её нету)
-    for (let j = 0; j < n - 1; j++)
+    for (let row = 0; row < n; row++)
     {
-        let toDeleteWall = Math.random() * (100 - 1) + 1;
-
-        if (toDeleteWall >= 50)
-        {
-            str[j + 1] = str[j];
-        }
+        process.stdout.write(arr[row] + ' ');
     }
-
-    // Множества
-    process.stdout.write('1');
-    for (let j = 0; j < n; j++)
-    {
-        // стена ставится в том случае, если элемент справа не входит в множество
-        // иначе остаётся проход
-        if (str[j] != str[j + 1])
-        {
-            process.stdout.write('1');
-        }
-        else
-        {
-            process.stdout.write('0');
-        }
-    }
-    process.stdout.write('1');
-    process.stdout.write('\n');
-
-    // Разделители
-    process.stdout.write('1');
-    for (let j = 0; j < n; j++)
-    {
-        if (str[j] != str[j + 1])
-        {
-            process.stdout.write('0');
-        }
-        else
-        {
-            process.stdout.write('1');
-        }
-    }
-    process.stdout.write('1');
 
     process.stdout.write('\n');
 }
 
-for (let i = 0; i < n + 2; i++)
+let n = 6;
+
+let arrSets = [];
+let uniqueValue = 0;
+
+for (let i = 0; i < n; i++)
 {
-    process.stdout.write('1');
+    arrSets[i] = uniqueValue;
+    uniqueValue++;
+}
+
+for (let row = 0; row < n; row++)
+{
+    let arrDashes = [];
+
+    // Случайным образом объединяем множества
+    for (let elem = 0; elem < n; elem++)
+    {
+        let rand = Math.random() * (100 - 1) + 1;
+
+        if (rand >= 50)
+        {
+            arrSets[elem + 1] = arrSets[elem];
+        }
+    }
+
+    for (let i = 0; i < n; i++)
+    {
+        if (arrSets[i] != arrSets[i - 1])
+        {
+            arrDashes[i] = '0';
+        }
+        else
+        {
+            arrDashes[i] = '1';
+        }
+    }
+
+    // printArr(arrSets);
+    for (let i = 0; i < n; i++)
+    {
+        if (arrDashes[i] != arrDashes[i - 1])
+        {
+            process.stdout.write('1 ');
+        }
+        else
+        {
+            process.stdout.write('0 ');
+        }
+    }
+    process.stdout.write('\n');
+    printArr(arrDashes);
+
+    for (let i = 0; i < n; i++)
+    {
+        if (arrDashes[i] == '1')
+        {
+            arrSets[i] = uniqueValue;
+            uniqueValue++;
+        }
+    }
+    // printArr(arrSets);
+
 }
